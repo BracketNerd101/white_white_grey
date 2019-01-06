2f6208a8d0f9b8d65361c8e646444f6bd15637ad
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2019-01-05 23:40:08 -0500

    small js and html style

--- js/side_nav_custom.js
+++ js/side_nav_custom.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "65vw";
+  document.getElementById("mySidenav").style.width = "100vw";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
